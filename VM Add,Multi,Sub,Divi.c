1b143086e8939dccbed75cc088c2b3599b102eb8
vishvamoradiya24 <vishvamoradiya24@gmail.com>
2025-09-08 21:37:07 +0530

    Add files via upload

---
+++ VM Add,Multi,Sub,Divi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ float a,b,add,sub,mul,divi;
+ printf("Enter two numbers:");
+ scanf("%f%f",&a,&b);
+ add=a+b;
+ sub=a-b;
+ mul=a*b;
+ divi=a/b;
+ printf("\n Addition is %f",add);
+ printf("\n Subtraction is %f",sub);
+ printf("\n Multiplication is %f",mul);
+ printf("\n Division is %f",divi);
+ }
+
