e63789129fd2b465644eb3694c9542719ccb3a5d
vishvamoradiya24 <vishvamoradiya24@gmail.com>
2025-09-08 22:13:46 +0530

    Add files via upload

---
+++ VM Smallest value.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ printf("Enter three numbers:");
+ scanf("%d%d%d",&a,&b,&c);
+ if(a<=b&&a<=c)
+ {
+ printf("Smallest number is %d",a);
+ }
+ else if(b<=a&&b<=c)
+ {
+ printf("Smallest number is %d",b);
+ }
+ else
+ printf("Smallest number is %d",c);
+ }
